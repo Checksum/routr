1d64ada0300d857d5facb77a887033ed0aa03736
Srinath <srinath@iambot.net>
2012-02-25 22:40:57 +0800

    version 0.2

--- README.md
+++ README.md
@@ -1,20 +1,91 @@
-Route based initialization for JavaScript
+### Routr - Route specific initialization for JavaScript
 
-Example Usage:
+Routr is a lightweight JS library for route specific initialization functions. 
+
+In a modern website with lots of routes, running all the JS initialization code irrespective of route is slow and inefficient. Whereas plenty of solutions are available for apps that rely on hashbangs (#) in their routes, not many are available for "traditional" routes. Hence Routr.
+
+Please note that this is only client side routing and NOT server side!
+
+You can see a demo [here](http://iambot.net/demo/routr/)
+
+#### Installation
+
+Include the JS in the head section of your html
+
+```html
+<script src="routr.js"></script>
+```
+
+#### Usage
+
+1) Initialize your global path prefix if your application is not in the root directory. Ex: If your app runs at http://example.com/app/ then set the root to /app
+
+```javascript
+Routr.root("/app");
+```
+
+2) Add routes by calling the Routr.bind() method. This method takes 4 arguments:
+
+<table>
+	<tr>
+		<th>Argument</th>
+		<th>Type</th>
+		<th>Description</th>
+	</tr>
+	<tr>
+		<td>Route</td>
+		<td>String/RegExp</td>
+		<td>The route to bind to. Can be a string like "/home" or a RegExp object like /home/ (without the quotes)</td>
+	</tr>
+	<tr>
+		<td>Callback</td>
+		<td>Function</td>
+		<td>The callback function for the route. Put all your initialization code in here</td>
+	</tr>
+	<tr>
+		<td>Chaining</td>
+		<td>Boolean</td>
+		<td>If true, matches and executes all the parent routes as well. For example if chaining is set to true for the route "/group/home", then the route "/group" is run as well</td>
+	</tr>
+	<tr>
+		<td>Parameters</td>
+		<td>Array</td>
+		<td>The parameter names as an array. If using RegExp in the route, then the matched parameters will be available in the callback method. Please look at the example for more details</td>
+	</tr>
+</table>
+
+3. Add optional common before and after callback functions which are run for every route (if necessary)
 
 ```javascript
 Routr
-.bind("/", function() {
-		console.log("index");
+.before(function() {
+	console.log("I will be run before the callback for every matched route");
 })
-.bind("/home", function() {
-		console.log("home");
-})
-.bind("/home/news", function() {
-		console.log("home news");
-}, true)
-.bind("/home/news/new", function() {
-		console.log("home news new");
-}, true)
-.run();
+.after(function() {
+	console.log("And I will be run after the callback for every matched route");
+});
 ```
+
+4. Run the app by calling Routr.run();
+
+#### Example
+
+```javascript
+Routr
+	.bind("/", function() {
+		console.log("index");
+	})
+	.bind("home", function() {
+		console.log("home");
+	})
+	.bind("/group", function() {
+		console.log("group");
+	})
+	.bind(/\/group\/(\w+)\/(\d+)/, function() {
+		// The params defined will be available in this scope
+		console.log("group "+this.group+", "+this.id);
+	}, true, ['group','id'] );
+
+	Routr.run();
+```
+
--- routr.js
+++ routr.js
@@ -1,49 +1,144 @@
 /**
- * Routr
- * Route based initialization for JS
+ * Routr - Route specific initialization for JavaScript
  *
- * Author  : Srinath
- * Code    : https://github.com/Checksum/routr
- * Version : 0.1a
- * Date    : Feb 4, 2012
+ * Version	: 0.2
+ * Date			: Feb 25, 2012
+ * Author  	: Srinath
+ * Source		: http://github.com/Checksum/routr
+ * Demo			: http://iambot.net/demo/routr/
  *
  */
 var Routr = function() {
-	var	fns = {},
-		common_fn,
-		base_url,
-		called = false;
+	var	base_url,
+			fns = {},
+			before_fn,
+			after_fn;
+	
+	// Simple test to check if the path is a regex
+	function isRegex(path) {
+		return typeof path === 'object' && typeof path.exec === 'function';
+	}
+	
+	// Sanitizes our path by adding a / to the front and deleting
+	// the trailing / if any
+	function sanitize(path) {
+		if( path == '/') {
+			return path;
+		}
+		if( path.charAt(0) !== '/' ) {
+			path = '/'+path;
+		}
+		if( path.charAt(path.length - 1) == '/' ) {
+			path = path.substring(0, path.length - 1);
+		}
+		return path;
+	}
 
+	// Our magic starts here
 	return ({
+		// Set the root path for the application
+		// Ex: if your app is at http://example.com/app/ then
+		// set the url as /app
 		root: function(url) {
-			base_url = url;
+			base_url = sanitize(url);
 			return this;
 		},
-		common: function(fn) {
-			common_fn = fn;
+
+		// Common function to execute before the paths are matched
+		before: function(fn) {
+			before_fn = fn;
 			return this;
 		},
-		bind: function(route, fn, chain) {
+
+		// Common function to execute after the paths are matched
+		after: function(fn) {
+			after_fn = fn;
+			return this;
+		},
+
+		// Bind paths
+		// @route - can be a string or regex pattern ("/home" or /home/)
+		// @fn 		- the callback function
+		// @param - an array with the names of the parameters ['group','id']
+		// @chain - if true, all the matching parent paths are executed
+		bind: function(route, fn, chain, param) {
+			// If our route is a regex patten, extract the patten string
+			if (isRegex(route)) {
+				route = route.source;
+			}
+			// Otherwise, sanitize our route
+			else {
+				route = sanitize(route);
+			}
+			// Store our stuff for the route
 			fns[route] = {};
 			fns[route].fn = fn;
+			fns[route].param = param;
 			fns[route].chain = chain ? true : false;
 			return this;
 		},
+
+		// Run our app
 		run: function() {
-			var path = document.location.pathname;
-			if( base_url && base_url !== '' ) path = path.replace(base_url, '');
-			var route = fns[path];
-			if( route.chain ) {
-				var slugs = path.split("/"),
-					slug = '';
-				for( var i=1; i<slugs.length; i++ ) {
-					slug += '/'+slugs[i];
-					if( fns[slug] && typeof fns[slug].fn === 'function' ) fns[slug].fn.call();
-				}
-				called = true;
+			var path = document.location.pathname,
+					route,
+					param,
+					params = {},
+					i	= 0,
+					regex,
+					paramName;
+
+			// Get our pathname and remove the base_url if set
+			if( base_url && base_url !== '' ) {
+				path = path.replace(base_url, '');
 			}
-			if( !called && typeof fns[path].fn === 'function' ) route.fn.call();
-			common_fn && common_fn.call();
+			// Check if each route we have matches the path
+			for( route in fns ) {
+				if( fns.hasOwnProperty(route) ) {
+					regex = '^'+route+'\\/?$';
+					var matches = path.match(regex);
+					if( matches ) {
+						// We don't need the first match as its the whole string
+						path = matches.shift();
+						for( i=0; i<matches.length; i++ ) {
+							paramName = fns[route].param && fns[route].param[i] || 0;
+							params[paramName] = matches[i];
+						}
+						// Now, if the parameter names array is sent, we are
+						// directly placing the variables in the function scope
+						// Ex: if ['name','number'] is sent then we can access its
+						// values inside the function as this.name and this.number.
+						// Else, we can access the entire array as this.param[0]
+						if( fns[route].param && fns[route].param.length == matches.length ) {
+							for( param in params ) {
+								this[param] = params[param];
+							}
+						}
+						else {
+							this.param = params;
+						}
+		
+						// Common before fn
+						before_fn && before_fn.apply(this);
+						
+						// Matching and executing all our parent paths if chaining
+						if( fns[route].chain ) {
+							var slugs = path.split("/"),
+									slug = '';
+							for( var i=1; i<slugs.length-1; i++ ) {
+								slug += '/'+slugs[i];
+								fns[slug] && fns[slug].fn && fns[slug].fn.call(this);
+							}
+						}
+						
+						// Main route callback function
+						fns[route].fn && fns[route].fn.apply(this);
+						
+						// Common after fn
+						after_fn && after_fn.apply(this);
+					}
+				}
+			}
 		}
 	});
 }();
